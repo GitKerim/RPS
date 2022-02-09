3a70ded6925a55186d96ca6916bd5ff7309504e1
kerim <kerimomerbegovic@hotmail.com>
2022-02-09 20:35:46 +0100

    Fixed playerPlay function

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+let computerPick = computerPlay();
+let playerPick = playerPlay();
+
+//Returns random computer play
 function computerPlay(){
-    let computerOptions = ["Rock","Paper","Scissors"];
+    let computerOptions = ["rock","paper","scissors"];
       
       let randomComputerOptions = computerOptions[Math.floor(Math.random()*computerOptions.length)];
       return randomComputerOptions;
+      let computerResult = computerPlay;
 }
-function playerSelection(){
-  let playerInput = prompt("Choose 'ur hand");
-  playerInput = playerInput.toLowerCase();
-  if (playerInput ==="rock" || "paper" || "scissors"){
-    return playerInput;}
+//Asks for players hand
+function playerPlay(){
+  let playerInput = prompt("Choose 'ur hand").toLowerCase();
+  if (playerInput === 'rock' || playerInput ==='paper' || playerInput=== 'scissors'){   
+    return playerInput;
+    }
     else {
-      return "Please pick Rock, Paper or Scissors"
+      return "Please pick Rock, Paper or Scissors";
         }
 }
-console.log(playerSelection())
+
