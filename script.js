6dea6b76bcedb5b9830d9351a18616f008036d24
kerim <kerimomerbegovic@hotmail.com>
2022-02-09 21:35:32 +0100

    Added playRound function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let computerPick = computerPlay();
 let playerPick = playerPlay();
+let playedRound = playRound();
 
 //Returns random computer play
 function computerPlay(){
@@ -19,4 +20,24 @@ function playerPlay(){
       return "Please pick Rock, Paper or Scissors";
         }
 }
+function playRound(){
+  if (playerPick === "rock" && computerPick === "scissors" ){
+    return "Player wins - Rock beats scissors";
+  }else if (playerPick ==="paper" && computerPick === "rock"){
+    return "Player wins - Paper beats Rock";
+  }else if(playerPick === "scissors" && computerPick === "paper"){
+    return "Player wins - Scissors beat Paper";
+  }else if (computerPick === "rock" && playerPick === "scissors" ){
+    return "Computer wins - Rock beats scissors";
+  }else if (computerPick ==="paper" && playerPick === "rock"){
+    return "Computer wins - Paper beats Rock";
+  }else if(computerPick === "scissors" && playerPick === "paper"){
+    return "Computer wins - Scissors beat Paper";
+  }else if (computerPick === playerPick){
+    return "Tie";
+  }
 
+}
+console.log(computerPick);
+console.log(playerPick);
+console.log(game());
