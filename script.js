bb07ffc36d5e586a437423d1ab4f88b6663af9b8
kerim <kerimomerbegovic@hotmail.com>
2022-02-25 06:36:21 +0100

    CSS

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
+let game;
+let playerScore = 0;
+let computerScore = 0;
+let winner;
 let computerPick = computerPlay();
-let playerPick = playerPlay();
-let playedRound = playRound();
+let playerPick;
+let roundWinner;
 
-//Returns random computer play
-function computerPlay(){
-    let computerOptions = ["rock","paper","scissors"];
-      
-      let randomComputerOptions = computerOptions[Math.floor(Math.random()*computerOptions.length)];
-      return randomComputerOptions;
-      let computerResult = computerPlay;
-}
-//Asks for players hand
-function playerPlay(){
-  let playerInput = prompt("Choose 'ur hand").toLowerCase();
-  if (playerInput === 'rock' || playerInput ==='paper' || playerInput=== 'scissors'){   
-    return playerInput;
-    }
-    else {
-      return "Please pick Rock, Paper or Scissors";
-        }
-}
-function playRound(){
-  if (playerPick === "rock" && computerPick === "scissors" ){
-    return "Player wins - Rock beats scissors";
+
+function round(){
+  if (computerPick === playerPick){
+    roundWinner = "Tie";}
+  if(playerPick === "rock" && computerPick === "scissors" ){
+    return "Player wins - Rock beats scissors"; 
   }else if (playerPick ==="paper" && computerPick === "rock"){
     return "Player wins - Paper beats Rock";
   }else if(playerPick === "scissors" && computerPick === "paper"){
     return "Player wins - Scissors beat Paper";
-  }else if (computerPick === "rock" && playerPick === "scissors" ){
+  }playerScore=playerScore++;
+  if (computerPick === "rock" && playerPick === "scissors" ){
     return "Computer wins - Rock beats scissors";
   }else if (computerPick ==="paper" && playerPick === "rock"){
     return "Computer wins - Paper beats Rock";
   }else if(computerPick === "scissors" && playerPick === "paper"){
     return "Computer wins - Scissors beat Paper";
-  }else if (computerPick === playerPick){
-    return "Tie";
-  }
-
+  }computerScore=computerScore++;
+} 
+function computerPlay(){
+    let computerOptions = ["rock","paper","scissors"];
+      
+      let randomComputerOptions = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+      return randomComputerOptions;
 }
-console.log(computerPick);
-console.log(playerPick);
-console.log(game());
+playerPick = "rock";
